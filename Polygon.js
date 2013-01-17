031b10e2527d8f9e030de32a80799738d7ffb1e5
Wojciech Pietrzak <act.as.coder@gmail.com>
2013-01-17 22:45:48 +0100

    polygon lines

--- Polygon.js
+++ Polygon.js
@@ -3,6 +3,13 @@ var Polygon = function(coords){
 };
 
 Polygon.prototype.lines = function(){
-  return [];
+  var size = this.coords.length;
+  var out = [];
+  for(var i = 0; i < size-1; i++){
+    console.log(this.coords[i]);
+    out.push(new Line([this.coords[i][0], this.coords[i][1]], [this.coords[i+1][0], this.coords[i+1][1]]));
+  }
+  out.push(new Line([this.coords[size-1][0],this.coords[size-1][1]],[this.coords[0][0], this.coords[0][1]]));
+  return out;
 };
 
